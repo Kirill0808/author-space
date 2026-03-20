dbca83dad0d358ad4a49fdf7d8b55a541a199fcd
kirill0808 <dronov0808@gmail.com>
2026-03-20 23:01:22 +0200

    feat: complete Phase 1 and Phase 2 basic setups (Prisma, Auth, Shadcn, seed)

---
+++ src/lib/prisma.ts
@@ -0,0 +1,29 @@
+import dns from 'node:dns'
+dns.setDefaultResultOrder('ipv4first')
+
+import { PrismaClient } from '@prisma/client'
+
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const prismaClientSingleton = () => {
+  const u = new URL(process.env.DIRECT_URL!)
+  const pool = new Pool({
+    host: u.hostname,
+    port: parseInt(u.port),
+    user: u.username,
+    password: decodeURIComponent(u.password),
+    database: u.pathname.slice(1),
+    ssl: { rejectUnauthorized: false },
+  })
+  const adapter = new PrismaPg(pool as any)
+  return new PrismaClient({ adapter })
+}
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
